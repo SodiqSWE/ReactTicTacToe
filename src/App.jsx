25f0c0342523a4ebda3f96e0a4586897d886950a
SodiqSWE <sodiq260@gmail.com>
2025-04-01 12:11:37 -0400

    initialized the game container with player 1 and 2 placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 function App() {
-  return <p>COMING SOON...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">PLAYER 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">PLAYER 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
