8f31a123739ad5eac0d0e84594818eb14539b5cf
SodiqSWE <sodiq260@gmail.com>
2025-04-28 09:49:02 -0400

    added conditional button caption

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,20 +4,24 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+    btnCaption = "Save";
   }
 
   return (
     <li>
       <span className="player">
-        {isEditing ? (
-          <input type="text" required />
-        ) : (
-          <span className="player-name">{name}</span>
-        )}
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 }
